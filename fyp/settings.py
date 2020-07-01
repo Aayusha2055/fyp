d112b871d96f0da520c5233187ff7713b1bd8d25
kusalb <grand.kusal.kb@gmail.com>
2020-07-01 09:33:52 +0545

    Crawling added

--- fyp/settings.py
+++ fyp/settings.py
@@ -74,19 +74,7 @@ WSGI_APPLICATION = 'fyp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fyp',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-}
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
