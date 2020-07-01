7a7eb948c8a7251fbdcbd856fd953029ce01aec1
Aayusha Paudel <aayusha2055@gmail.com>
2020-07-01 22:49:01 +0545

    changes made on ui

---
+++ account/static/accounts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+            $(".navbar").css("background", "white");
+        } else {
+            $(".navbar").css("background", "none");
+        }
+    })
+})
